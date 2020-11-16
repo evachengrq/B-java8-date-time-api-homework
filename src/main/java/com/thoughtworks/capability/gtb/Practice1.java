76482b9e3a6b20b33643cc0366bff91965e41dbc
Ruoqian Cheng <ruoqian.cheng@RuoqiandeMacBook-Pro.local>
2020-11-16 23:34:55 +0800

    [evacheng]feat: passed practice 1

--- src/main/java/com/thoughtworks/capability/gtb/Practice1.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice1.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 
 /**
  * 计算任意日期与下一个劳动节相差多少天
@@ -11,6 +14,20 @@ import java.time.LocalDate;
 public class Practice1 {
 
   public static long getDaysBetweenNextLaborDay(LocalDate date) {
-    return 0;
+    int year = date.getYear();
+    int month = date.getMonthValue();
+    int day = date.getDayOfYear();
+
+    int monthOfLabourDay = 5;
+    int dayOfLabourDay = 1;
+
+    LocalDate labourDay;
+    if (month < monthOfLabourDay) {
+      labourDay = LocalDate.of(year, monthOfLabourDay, dayOfLabourDay);
+    } else {
+      labourDay = LocalDate.of(year + 1, monthOfLabourDay, dayOfLabourDay);
+    }
+
+    return(labourDay.toEpochDay() - date.toEpochDay());
   }
 }
